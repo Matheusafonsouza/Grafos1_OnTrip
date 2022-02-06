dbfba24e6c3f27bd90aff0478315f29f7b7b9a76
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-06 01:54:17 -0300

    basic form

--- web/src/components/Drawer/index.js
+++ web/src/components/Drawer/index.js
@@ -1,31 +1,55 @@
-import { Box, Flex, Heading, Select, Button } from '@chakra-ui/react';
+import { Box, Flex, Image, Select, Button } from "@chakra-ui/react";
+import logo from "../../assets/logo.png"
 
 export default function Drawer() {
   return (
-    <Flex direction="column" align="center">
-      <Heading>Bem vindo ao On Trip!</Heading>
+    <Flex
+      direction="column"
+      align="center"
+      height="100%"
+      width="100%"
+      justify="space-around"
+    >
+      <Box marginTop="40px">
+        <Image src={logo} />
+      </Box>
 
-      <Box>
+      <Box flex="1"
+        width="100%"
+        padding="30px"
+      >
         <form>
           <Flex
             direction="column"
             align="center"
-            justifyContent="space-between"
+            width="100%"
           >
-            <Select placeholder='Select option'>
-              <option value='option1'>Option 1</option>
-              <option value='option2'>Option 2</option>
-              <option value='option3'>Option 3</option>
+            <Select
+              placeholder="Onde você está?"
+              marginBottom="40px"
+              width="100%"
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
             </Select>
-            <Select placeholder='Select option'>
-              <option value='option1'>Option 1</option>
-              <option value='option2'>Option 2</option>
-              <option value='option3'>Option 3</option>
+            <Select
+              placeholder="Para onde quer ir?"
+              marginBottom="40px"
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
             </Select>
-            <Button colorScheme='blue'>Button</Button>
+            <Button
+              colorScheme="blue"
+              isFullWidth
+            >
+              Button
+            </Button>
           </Flex>
-        </form>
-      </Box>
+        </form >
+      </Box >
     </Flex >
   )
 }
